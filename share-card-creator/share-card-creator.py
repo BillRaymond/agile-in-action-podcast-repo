264eea8b0caea134179a2e750d25a9a4958e9fe3
Jens Kötterheinrich <jens@koetterheinri.ch>
2021-08-23 23:14:20 +0200

    fix try

--- share-card-creator/share-card-creator.py
+++ share-card-creator/share-card-creator.py
@@ -327,7 +327,7 @@ def getCLIParameters(argv):
     
     try:
         opts, args = getopt.getopt(argv,"h",paramArr)
-        for opt, arg in args:
+        for opt, arg in opts:
             opt=opt.lower()[2:] #2 cause --param, got it?
             if opt in ('h','help'):
                 print(
