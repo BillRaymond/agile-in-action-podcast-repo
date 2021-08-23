50140d3187de5e5d7ba7cc9ffa1bd7f265a5b838
Jens Kötterheinrich <jens@koetterheinri.ch>
2021-08-23 21:27:42 +0200

    fix

--- share-card-creator/shell.sh
+++ share-card-creator/shell.sh
@@ -1,9 +1,9 @@
 pwd
 
-python3 share-card-creator.py –title “four guests”  –photos “uploads/michele.jpg,uploads/yunpeng.jpg,uploads/matt-koball.jpg,uploads/albert.jpg” –guests “Michele Magnardini, Yunpeng Li, Matt Koball, and Albert Chen” –card_file “uploads/2021-08-17-four-guests.png”
+python3 share-card-creator.py -–title “four guests”  -–photos “uploads/michele.jpg,uploads/yunpeng.jpg,uploads/matt-koball.jpg,uploads/albert.jpg” -–guests “Michele Magnardini, Yunpeng Li, Matt Koball, and Albert Chen” –-card_file “uploads/2021-08-17-four-guests.png”
 
-python3 share-card-creator.py –title “Three guests”  –photos “uploads/loretta.jpg,uploads/farlik.jpg,uploads/brower-6917.jpg” –guests “Loretta Harland, Dr. John Farlik, and Dr. Tracy Brower” –card_file “uploads/2021-08-16-three-guests.png”
+python3 share-card-creator.py -–title “Three guests”  -–photos “uploads/loretta.jpg,uploads/farlik.jpg,uploads/brower-6917.jpg” -–guests “Loretta Harland, Dr. John Farlik, and Dr. Tracy Brower” -–card_file “uploads/2021-08-16-three-guests.png”
 
-python3 share-card-creator.py –title “two guests”  –photos “uploads/koning.jpg,uploads/maarten-1.jpeg” –guests “Peter Koning and maarten dalmijn” –card_file “uploads/2021-08-15-two-guests.png”
+python3 share-card-creator.py -–title “two guests”  -–photos “uploads/koning.jpg,uploads/maarten-1.jpeg” -–guests “Peter Koning and maarten dalmijn” -–card_file “uploads/2021-08-15-two-guests.png”
 
-python3 share-card-creator.py –title “One guest”  –photos “uploads/sarah-shewell.jpg” –guests “Sarah Shewell” –card_file “uploads/2021-08-14-one-guest.png”‚
+python3 share-card-creator.py -–title “One guest”  -–photos “uploads/sarah-shewell.jpg” -–guests “Sarah Shewell” –-card_file “uploads/2021-08-14-one-guest.png”‚
