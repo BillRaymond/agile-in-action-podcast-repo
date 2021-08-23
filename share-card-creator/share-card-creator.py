6110d65cf41ef1851797330717886ed72f71c0ba
Jens Kötterheinrich <jens@koetterheinri.ch>
2021-08-23 22:43:42 +0200

    logging

--- share-card-creator/share-card-creator.py
+++ share-card-creator/share-card-creator.py
@@ -327,7 +327,7 @@ def getCLIParameters(argv):
     
     try:
         opts, args = getopt.getopt(argv,"h",paramArr)
-        print(opts + ":" + args)
+        print(opts)
         for opt, arg in opts:
             print(opt + ":" + arg)
             opt=opt.lower()[2:] #2 cause --param, got it?
